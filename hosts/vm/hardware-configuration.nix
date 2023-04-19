710c436006d15dab5345402e1299b46fcb8c942b
aniketkharel <aniketkhareldev@gmail.com>
2023-04-19 19:52:43 +0545

    some networking config for vm

--- hosts/vm/hardware-configuration.nix
+++ hosts/vm/hardware-configuration.nix
@@ -15,8 +15,8 @@
 
   swapDevices = [ ];
 
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s3.useDHCP = lib.mkDefault true;
+  networking.useDHCP = lib.mkDefault false;
+  networking.interfaces.enp0s3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
