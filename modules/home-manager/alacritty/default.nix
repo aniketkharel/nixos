190f594364786253288fe1f5cda33545354ab484
aniketkharel <aniketkhareldev@gmail.com>
2023-04-19 13:40:49 +0545

    WIP for nvim config changes more🤼

--- modules/home-manager/alacritty/default.nix
+++ modules/home-manager/alacritty/default.nix
@@ -1,16 +1,20 @@
-# Font configuration
-font:
-  size: 12
-  offset:
-    x: 0
-    y: 0
-  glyph_offset:
-    x: 0
-    y: 0
-  normal:
-    family: Caskaydia Cove Nerd Font
-    style: Medium
-  bold:
-    family: Caskaydia Cove Nerd Font
-  italic:
-    family: Caskaydia Cove Nerd Font
+{
+  window.dimensions = {
+    lines = 3;
+    columns = 200;
+  };
+  key_bindings = [
+  {
+    key = "K";
+    mods = "Control";
+    chars = "\\x0c";
+  }
+  ];
+  font = {
+    size = 12;
+    normal = {
+      family = "Caskaydia Cove Nerd Font";
+      style = "Medium";
+    };
+  };
+}
