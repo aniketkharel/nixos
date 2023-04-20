2afcdf1bb5d032bbcc96b3178f5df60c8144a0f6
aniketkharel <aniketkhareldev@gmail.com>
2023-04-20 08:52:48 +0545

    templating config 4 🤺

--- flake.nix
+++ flake.nix
@@ -1,99 +1,34 @@
 {
-  description = "kratos new nix config";
-  inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+  description = "kratos collection of flake templates";
 
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  outputs = { self }: {
 
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
+    templates = {
 
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
-  };
-
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      inherit (self) outputs;
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-      user = "kratos";
-      user-vm = "kratos-vm";
-    in
-    rec {
-      templates = {
-        full = {
-          description = ''
-            kratos flake and starting point for my machines.
-            '';
-          path = .;
-        };
-      };
-      # Your custom packages
-      # Acessible through 'nix build', 'nix shell', etc
-      packages = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in import ./pkgs { inherit pkgs; }
-      );
-      # Devshell for bootstrapping
-      # Acessible through 'nix develop' or 'nix-shell' (legacy)
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in import ./shell.nix { inherit pkgs; }
-      );
-
-      # Your custom packages and modifications, exported as overlays
-      overlays = import ./overlays { inherit inputs; };
-      # Reusable nixos modules you might want to export
-      # These are usually stuff you would upstream into nixpkgs
-      nixosModules = import ./modules/nixos;
-      # Reusable home-manager modules you might want to export
-      # These are usually stuff you would upstream into home-manager
-      homeManagerModules = import ./modules/home-manager;
-
-      # NixOS configuration entrypoint
-      # Available through 'nixos-rebuild --flake .#your-hostname'
-      nixosConfigurations = {
-        vm = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [
-            ./hosts/vm/configuration.nix
-          ];
-        };
+      vm = {
+        path = ./vm;
+        description = "A very basic vm flake template.";
+        welcomeText = ''
+          You just created a vm flake template.
+          '';
       };
 
-      # Standalone home-manager configuration entrypoint
-      # Available through 'home-manager --flake .#your-username@your-hostname'
-      homeConfigurations = {
-        # maybe personal config
-        "${user}@${user}" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = { inherit inputs outputs; };
-          modules = [
-            ./home-manager/kratos.nix
-          ];
-        };
-        # vm configs
-        "${user-vm}@${user-vm}" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = { inherit inputs outputs; };
-          modules = [
-            ./home-manager/kratos-vm.nix
-          ];
-        };
+      vm = {
+        path = ./personal;
+        description = "A very basic personal machine flake template.";
+        welcomeText = ''
+          You just created a vm flake template.
+          '';
       };
+
+      vm = {
+        path = ./work;
+        description = "A very basic work machine flake template.";
+        welcomeText = ''
+          You just created a vm flake template.
+          '';
+      };
+
+      defaultTemplate = self.templates.vm;
     };
-}
+  }
