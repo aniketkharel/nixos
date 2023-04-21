9142a52ee7df3e14a3ea54816d62df0af8e1418c
aniketkharel <aniketkhareldev@gmail.com>
2023-04-21 15:21:10 +0545

    added rustup cargo conf 1

--- home-manager/kratos-vm.nix
+++ home-manager/kratos-vm.nix
@@ -50,7 +50,7 @@ in
     };
   };
 
-  home.packages = with pkgs; [ firefox flameshot alacritty exa bat tree-sitter gcc nodejs picom i3blocks rofi autotiling lazygit lazydocker rust-analyzer cargo rustup];
+  home.packages = with pkgs; [ firefox flameshot alacritty exa bat tree-sitter gcc nodejs picom i3blocks rofi autotiling lazygit lazydocker rust-analyzer rustup];
 
   programs = {
     git = {
