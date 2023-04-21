8761f2212bf2c5607d4d2b4700a8864bf77aa717
aniketkharel <aniketkhareldev@gmail.com>
2023-04-21 18:40:10 +0545

    testing overlays now

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,8 @@
 # This file defines overlays
 { inputs, ... }:
 {
-  # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs { pkgs = final; };
 
-  # This one contains whatever you want to overlay
-  # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
     # example = prev.example.overrideAttrs (oldAttrs: rec {
@@ -13,8 +10,6 @@
     # });
   };
 
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
       system = final.system;
