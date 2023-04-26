bd7e2006bfd8c8c844af3586e9a5ccb90d946431
aniketkharel <aniketkhareldev@gmail.com>
2023-04-26 20:14:42 +0545

    flake configs 🤞

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,10 +1,11 @@
 let lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
 in
-import (fetchTarball {
+import
+  (fetchTarball {
     url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
     sha256 = lock.narHash;
-    })
-(import (builtins.fetchTarball {
-         url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+  })
+  (import (builtins.fetchTarball {
+    url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
     sha256 = lock.narHash;
-         }))
+  }))
