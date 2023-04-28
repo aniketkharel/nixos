292c36d0b3132fcaee3a6643a534b3bce0f9622a
aniketkharel <aniketkhareldev@gmail.com>
2023-04-28 14:48:33 +0545

    font configs ❇

--- shared/default.nix
+++ shared/default.nix
@@ -1,48 +1,52 @@
-
-{config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
-    environment.systemPackages = with pkgs; [
-      aspell
-      aspellDicts.en
-      bind
-      docker
-      docker-compose
-      neovim
-      fd
-      feh
-      file
-      fzf
-      git
-      zsh
-      gnum4
-      gnupg
-      htop
-      imagemagick
-      scrot
-      lsof
-      man-pages
-      ncdu
-      psmisc
-      python3
-      ripgrep
-      rsync
-      shellcheck
-      smartmontools
-      stow
-      tmux
-      unzip
-      wget
-      curl
-      which
-      whois
-      sysstat
-      xclip
-      pkg-config
-      openssl
-      zoxide
-      marksman
-    ];
+  environment.systemPackages = with pkgs; [
+    aspell
+    aspellDicts.en
+    bind
+    docker
+    docker-compose
+    neovim
+    fd
+    feh
+    file
+    fzf
+    git
+    zsh
+    gnum4
+    gnupg
+    htop
+    imagemagick
+    scrot
+    lsof
+    man-pages
+    ncdu
+    psmisc
+    python3
+    ripgrep
+    rsync
+    shellcheck
+    smartmontools
+    stow
+    tmux
+    unzip
+    wget
+    curl
+    which
+    whois
+    sysstat
+    xclip
+    pkg-config
+    openssl
+    zoxide
+    marksman
+  ];
 
-    # system shell
-    environment.shells = with pkgs; [zsh];
+  # system shell
+  environment.shells = with pkgs; [ zsh ];
+
+  # fonts
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "CascadiaCode" ]; })
+  ];
 }
