1d3075cf37eff756844430fb3114f1b327f55a03
aniketkharel <aniketkhareldev@gmail.com>
2023-06-20 22:24:20 +0545

    zsh WIP 2

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -72,7 +72,7 @@ in {
 
   users.users = {
     aniketdev = {
-      shell = pkgs.fish;
+      shell = pkgs.zsh;
       initialPassword = "tt";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [ ];
